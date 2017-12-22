a0bdbd0a2c513c2829e99abd42e06db06bbac87b
Fredrik Rahn <rahn.fredrik@gmail.com>
2017-12-22 02:38:52 +0100

    Removed relative imports

--- byzantine_server/__init__.py
+++ byzantine_server/__init__.py
@@ -2,5 +2,6 @@
 Init all modules
 '''
 
-from byzantine_server.server import ByzantineServer, RequestHandler
-from byzantine_server.byzantine import vote_round1, vote_round2
+from server import ByzantineServer, RequestHandler
+from byzantine import vote_round1, vote_round2
+from request_path import extract_ep

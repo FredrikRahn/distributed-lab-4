18dae384b238da8f20d990e6017812281bc6cb4a
Fredrik Rahn <rahn.fredrik@gmail.com>
2017-12-23 03:59:48 +0100

    Added models

---
+++ byzantine_agreement/models.py
@@ -0,0 +1,28 @@
+'''
+Models module
+'''
+
+def vote_data(node_id, vote):
+    '''
+    Vote model
+    '''
+
+    model = {
+        'node_id': node_id,
+        'vote': vote
+    }
+
+    return model
+
+def byzantine_vote(no_round, no_nodes, no_loyal, on_tie):
+    '''
+    Byzantine vote model
+    '''
+    model = {
+        'no_round': no_round,
+        'no_nodes': no_nodes,
+        'no_loyal': no_loyal,
+        'on_tie': on_tie
+    }
+
+    return model

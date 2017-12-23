1faa0d44d7e743f7ad0cff597f5b067f94f6ee1d
Fredrik Rahn <rahn.fredrik@gmail.com>
2017-12-23 04:04:49 +0100

    added vote func to handle voting

--- byzantine_agreement/generals/byzantine.py
+++ byzantine_agreement/generals/byzantine.py
@@ -3,11 +3,33 @@ Byzantine class
 '''
 from general import General
 
+
 class Byzantine(General):
+    """
+    Byzantine class
+        :param General: Parent class
+    """
 
     def __init__(self):
-        General.__init__(self)
-        self.profile = 'Byzantine'
+        # Run init on extended General class
+        super(Byzantine, self).__init__()
+        self.myProfile = 'Byzantine'
+
+    def vote(self, voting_data):
+        """
+        Call the respective function depending on the round
+            :param no_round: round number
+            :param no_loyal: number of loyal nodes
+            :param no_total: total number of nodes
+            :param on_tie: action on tie
+        """
+
+        if voting_data.no_round == 1:
+            self.vote_round1(voting_data.no_loyal, voting_data.on_tie)
+        elif voting_data.no_round == 2:
+            self.vote_round2(voting_data.no_loyal, voting_data.no_total, voting_data.on_tie)
+        else:
+            raise ValueError, 'Invalid round count'
 
     def vote_round1(self, no_loyal, on_tie):
         """

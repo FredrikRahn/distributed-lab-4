e3f99612968c273e15e349b963b10ccd18f81f4b
Fredrik Rahn <rahn.fredrik@gmail.com>
2017-12-23 04:06:40 +0100

    added docstrings to funcs and super() to init

--- byzantine_agreement/generals/honest.py
+++ byzantine_agreement/generals/honest.py
@@ -5,35 +5,45 @@ from random import randint
 
 from general import General
 
+
 class Honest(General):
+    """
+    Honest class
+        :param General: Parent class
+    """
 
     def __init__(self):
-        General.__init__(self)
-        self.profile = 'Honest'
-        self.vote = self.decide_vote()
-
-        if self.vote == 'Attack':
-            self.vote_attack()
-        elif self.vote == 'Retreat':
-            self.vote_retreat()
-        else:
-            raise TypeError('Vote failed. Unknown type')
+        # Run init on extended General class
+        super(Honest, self).__init__()
+        self.myProfile = 'Honest'
 
     def vote_attack(self):
+        """
+        TODO: Implement
+            :param self: temp
+        """
         print 'Voted Attack'
         return True
 
     def vote_retreat(self):
+        """
+        TODO: Implement
+            :param self: temp
+        """
         print 'Voted Retreat'
         return False
 
-    def decide_vote(self):
-        #TODO: Add logic on how to decide votes
+    def vote(self):
+        """
+        TODO: Implement
+            :param self: temp
+        """
+
+        # TODO: Add logic on how to decide votes
         # For now just do dice roll with 50/50 chance
 
-        roll = randint(1,100)
+        roll = randint(1, 100)
         if roll <= 50:
             return 'Attack'
         else:
             return 'Retreat'
-    

90317a01af824540b9ca7385518fad4ace0b1794
Fredrik Rahn <rahn.fredrik@gmail.com>
2017-12-23 04:04:16 +0100

    removed absolute imports

--- byzantine_agreement/generals/__init__.py
+++ byzantine_agreement/generals/__init__.py
@@ -1,3 +1,3 @@
-from __future__ import absolute_import
-from byzantine_agreement.generals.honest import Honest
-from byzantine_agreement.generals.byzantine import Byzantine
+from general import General
+from honest import Honest
+from byzantine import Byzantine

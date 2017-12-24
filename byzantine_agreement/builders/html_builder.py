6564cbb448d6aec3bf5f319d4a69366bd1b14059
Fredrik Rahn <rahn.fredrik@gmail.com>
2017-12-24 11:19:06 +0100

    Now extend object, added profile page

--- byzantine_agreement/builders/html_builder.py
+++ byzantine_agreement/builders/html_builder.py
@@ -4,28 +4,32 @@ This module assembles the HTML
 
 import os
 
-#Global vars
-#Save path to resources directory
+# Global vars
+# Save path to resources directory
 PATH_RESOURCES = os.path.dirname(os.path.realpath(__file__)) + '/resources/'
 
-class HtmlBuilder():
+
+class HtmlBuilder(object):
     '''
     This class handles the building of HTML
     '''
+
     def __init__(self):
         '''
         Init the HTML builder
         '''
         self.index = open(PATH_RESOURCES + 'index.html', 'r').read()
-        self.vote_result = open(PATH_RESOURCES + 'vote_result.html', 'r').read()
+        self.profile = open(PATH_RESOURCES + 'profile.html', 'r').read()
+        self.vote_result = open(
+            PATH_RESOURCES + 'vote_result.html', 'r').read()
 
-    def build_page(self):
+    def build_page(self, profile):
         ''' 
         Assemble page HTML
         '''
-        return self.index
-        
-    
+        page = self.profile % profile + self.index
+        return page
+
     def build_vote_result(self, results):
         '''
         Updates the voting result template

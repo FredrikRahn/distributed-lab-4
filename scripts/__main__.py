491ef6e21545dd508d467a4c81c3a2519f142ba9
Fredrik Rahn <rahn.fredrik@gmail.com>
2017-12-27 00:29:22 +0100

    Testing

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,23 +1,18 @@
 import sys
+from lab1 import Lab1
+from resources import test_multiple_instances_path
 import webbrowser
-from lab4 import Lab4
-from __init__ import PATH_RESOURCES
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print 'script_to_run omitted'
-    else:
-        script_to_run = sys.argv[1]
 
-        if script_to_run == 'lab4':
-            # Instantiate Lab4 class
-            script = Lab4()
-            if len(sys.argv) == 3:
-                if sys.argv[2] == 'show':
-                    # User want to open the webpage as well
-                    webbrowser.open(PATH_RESOURCES + 'multiple_instances.html')
-            script.run()
-        else:
-            print 'script not found!'
-
-
+	if len(sys.argv) < 2:
+		print('Arguments: script_to_run')
+	else:
+		script_name = sys.argv[1]
+		if script_name == 'lab1':
+			script = Lab1()
+			if len(sys.argv) > 2 and sys.argv[2] == 'display':
+				webbrowser.open(test_multiple_instances_path)
+			script.run()
+		else:
+			print('Could not find script {}'.format(script_name))

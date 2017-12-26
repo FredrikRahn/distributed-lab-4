cddf76357e43c606b9951798d2ac08de5f6845af
Fredrik Rahn <rahn.fredrik@gmail.com>
2017-12-26 23:35:48 +0100

    Updated logic for running scripts

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -12,9 +12,10 @@ if __name__ == '__main__':
         if script_to_run == 'lab4':
             # Instantiate Lab4 class
             script = Lab4()
-            if sys.argv[2] and sys.argv[2] == 'show':
-                # User want to open the webpage as well
-                webbrowser.open(PATH_RESOURCES + 'multiple_instances.html')
+            if len(sys.argv) == 3:
+                if sys.argv[2] == 'show':
+                    # User want to open the webpage as well
+                    webbrowser.open(PATH_RESOURCES + 'multiple_instances.html')
             script.run()
         else:
             print 'script not found!'

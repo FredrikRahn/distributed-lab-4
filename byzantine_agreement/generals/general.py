98a11daf5b4d78ed6432706197b452981734e7cb
Fredrik Rahn <rahnf@student.chalmers.se>
2017-12-28 17:18:11 +0100

    For now commented out choose_role and shared vars needed for choose_role

--- byzantine_agreement/generals/general.py
+++ byzantine_agreement/generals/general.py
@@ -11,65 +11,65 @@ class General(object):
     """
     # Vars shared by all Generals
     lock = Lock()
-    # For now set #Byzantine allowed to 1
-    nrByzantineAllowed = 1
-    # Keep track of how many generals that has chosen Byzantine
-    byzantine_chosen = 0
-    # Numbers rolled, to force last node into Byzantine if noone has chosen it
-    times_rolled = 0
+    # # For now set #Byzantine allowed to 1
+    # nrByzantineAllowed = 1
+    # # Keep track of how many generals that has chosen Byzantine
+    # byzantine_chosen = 0
+    # # Numbers rolled, to force last node into Byzantine if noone has chosen it
+    # times_rolled = 0
 
     def __init__(self):
         # Init profile to None
-        self.profile = None
+        self.my_profile = 'General'
 
-    def choose_role(self, numberOfNodes):
-        """
-        Chooses a role for a general
-            :param numberOfNodes: Number of nodes in the network
-        """
-        # Try and acquire shared general lock
-        General.lock.acquire()
+    # def choose_role(self, numberOfNodes):
+    #     """
+    #     Chooses a role for a general
+    #         :param numberOfNodes: Number of nodes in the network
+    #     """
+    #     # Try and acquire shared general lock
+    #     General.lock.acquire()
 
-        #If noone has chosen byzantine, force the last node to choose byzantine
-        if General.byzantine_chosen < 1 and General.times_rolled == numberOfNodes - 1:
-            General.times_rolled += 1
-            General.byzantine_chosen += 1
+    #     #If noone has chosen byzantine, force the last node to choose byzantine
+    #     if General.byzantine_chosen < 1 and General.times_rolled == numberOfNodes - 1:
+    #         General.times_rolled += 1
+    #         General.byzantine_chosen += 1
 
-            # Set profile, release lock and return
-            General.lock.release()
-            self.profile = 'Byzantine'
-            print 'Profile set as Byzantine'
-            return self.profile
+    #         # Set profile, release lock and return
+    #         General.lock.release()
+    #         self.profile = 'Byzantine'
+    #         print 'Profile set as Byzantine'
+    #         return self.profile
 
-        elif General.byzantine_chosen < General.nrByzantineAllowed:
-            roll = self.roll_dice(numberOfNodes)
-            General.times_rolled += 1
+    #     elif General.byzantine_chosen < General.nrByzantineAllowed:
+    #         roll = self.roll_dice(numberOfNodes)
+    #         General.times_rolled += 1
 
-            if roll < numberOfNodes / 2:
-                General.byzantine_chosen += 1
-                # Set profile, release lock and return
-                General.lock.release()
-                self.profile = 'Byzantine'
-                print 'Profile set as Byzantine'
-                return self.profile
-            else:
-                # Set profile, release lock and return
-                General.lock.release()
-                self.profile = 'Honest'
-                print 'Profile set as Honest'
-                return self.profile
-        else:
-            # Max amount of Byzantine reached
-            # Set profile to honest, release lock and return
-            General.lock.release()
-            self.profile = 'Honest'
-            print 'Profile set as Honest'
-            return self.profile
+    #         if roll < numberOfNodes / 2:
+    #             General.byzantine_chosen += 1
+    #             # Set profile, release lock and return
+    #             General.lock.release()
+    #             self.profile = 'Byzantine'
+    #             print 'Profile set as Byzantine'
+    #             return self.profile
+    #         else:
+    #             # Set profile, release lock and return
+    #             General.lock.release()
+    #             self.profile = 'Honest'
+    #             print 'Profile set as Honest'
+    #             return self.profile
+    #     else:
+    #         # Max amount of Byzantine reached
+    #         # Set profile to honest, release lock and return
+    #         General.lock.release()
+    #         self.profile = 'Honest'
+    #         print 'Profile set as Honest'
+    #         return self.profile
 
-    def roll_dice(self, numberOfNodes):
-        """
-        Roll the dice
-            :param numberOfNodes: #nodes is used as max roll
-        """
-        roll = randint(1, numberOfNodes)
-        return roll
+    # def roll_dice(self, numberOfNodes):
+    #     """
+    #     Roll the dice
+    #         :param numberOfNodes: #nodes is used as max roll
+    #     """
+    #     roll = randint(1, numberOfNodes)
+    #     return roll

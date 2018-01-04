04302a4e29f8b6c78cc9f60a0e8369f7b761cd75
Fredrik Rahn <rahn.fredrik@gmail.com>
2018-01-04 22:15:02 +0100

    Now properly parse sys.argv

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
                     # User want to open the webpage as well
                     webbrowser.open(PATH_RESOURCES + 'multiple_instances.html')
                     SCRIPT.run()
-                elif sys.argv[2].isdigit:
+                elif sys.argv[2].isdigit():
                     SCRIPT.run(sys.argv[2])
             elif len(sys.argv) == 4:
-                if sys.argv[2] == 'show' and sys.argv[3].isdigit:
+                if sys.argv[2] == 'show' and sys.argv[3].isdigit():
                     webbrowser.open(PATH_RESOURCES + 'multiple_instances.html')
                     SCRIPT.run(int(sys.argv[3]))
         elif SCRIPT_TO_RUN == 'show':
